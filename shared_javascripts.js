2af8d66b8fb170e8ee341d7c8468feecbb95674d
Griffin Buckley <GriffinBuckley@Gmail.com>
2014-04-26 22:37:16 -0400

    test

--- shared_javascripts.js
+++ shared_javascripts.js
@@ -74,6 +74,39 @@ function SetDeviceDisplay(){
     DeviceTypeDiv.innerHTML = DetectDevice().Type();
 };
 
+function CreateCookie(name, value, days, secure){
+    var secured = ";";
+    if (secure){
+        secured = secured + " secure;";
+    };
+    if (days){
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else {
+        var expires = "";
+    };
+    document.cookie = name + "=" + value+expires+"; path=/" + secured;
+};
+
+function ReadCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0){
+            return c.substring(nameEQ.length,c.length);
+        };
+    }
+    return null;
+};
+
+function EraseCookie(name){
+    CreateCookie(name, "", -1);
+};
+
 window.onload = function(){
     function LoadDateAndTime(){
         var DeviceInformationDiv = document.getElementById('DeviceInformation');
